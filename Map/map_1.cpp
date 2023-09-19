4302b004b757b20af6dff1581304a41672a95f82
MortezaKhandan <49358240+MortezaKhandan@users.noreply.github.com>
2023-09-19 14:52:56 +0800

    Add files via upload

---
+++ Map/map_1.cpp
@@ -0,0 +1,30 @@
+/*
+There are also several other types of maps available in C++, such as:
+
+std::unordered_map: An associative container that stores elements in an unordered way using a hash table. It provides constant-time average complexity for searching,
+ inserting, and deleting elements.
+std::multimap: Similar to std::map, but allows multiple elements with the same key.
+std::unordered_multimap: Similar to std::unordered_map, but allows multiple elements with the same key.
+
+std::map
+st::std::unordered_map
+std::multimap
+std::unordered_multimap
+
+*/
+
+// set 
+/*
+In C++, there are two types of set available in the standard library:
+
+std::set: This is an ordered set, which means that the elements are stored in a specific order defined by a comparison function (by default, the < operator).
+This type of set is implemented as a balanced binary search tree, typically a red-black tree.
+
+std::unordered_set: This is an unordered set, which means that the elements are not stored in any particular order. This type of set is implemented as a hash
+ table, where the elements are stored based on their hash values.
+
+Both types of sets provide similar functionality, but they have different performance characteristics and are suited to different use cases.
+
+std::set 
+std::unordered_set
+*/
